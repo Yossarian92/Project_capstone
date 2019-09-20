9be8e120d61d338a70d4ad921ebb4ed6781c3ba7
Yossarian92 <44999992+Yossarian92@users.noreply.github.com>
2019-09-20 23:37:07 +0900

    Modified Recognition

--- Face_Recog/Face_Recog.py
+++ Face_Recog/Face_Recog.py
@@ -36,6 +36,9 @@ cam.set(4, 320) # set video height
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 
+cnt = 0
+subprocess.call('amixer -c 1 cset numid=2 off', shell=True)
+
 while True:
     ret, img =cam.read()
 
@@ -48,6 +51,11 @@ while True:
         minSize = (int(minW), int(minH)),
        )
 
+    cnt = cnt + 1
+    if cnt == 180:
+        subprocess.call('amixer -c 1 cset numid=2 off', shell=True)
+        cnt = 0
+
     for(x,y,w,h) in faces:
 
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
@@ -58,13 +66,17 @@ while True:
         if(confidence <= 50):
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
-			
+
+            subprocess.call('amixer -c 1 cset numid=2 on', shell=True)
+
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+
+
 
     cv2.imshow('camera',img) 
 
